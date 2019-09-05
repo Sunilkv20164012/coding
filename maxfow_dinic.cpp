1202f2e7419dd09ae8eedca039eb378d41335166
Sunil <sunilkv20164012@gmail.com>
2019-09-06 01:08:26 +0530

    Create maxfow_dinic.cpp

---
+++ maxfow_dinic.cpp
@@ -0,0 +1,45 @@
+template<class V> class MaxFlow_dinic {
+public:
+	struct edge { int to,reve;V cap;};
+	static const int MV = 16000;
+	vector<edge> E[MV];
+	int itr[MV],lev[MV];
+	void add_edge(int x,int y,V cap,bool undir=false) {
+		E[x].push_back((edge){y,(int)E[y].size(),cap});
+		E[y].push_back((edge){x,(int)E[x].size()-1,undir?cap:0});
+	}
+	void bfs(int cur) {
+		MINUS(lev);
+		queue<int> q;
+		lev[cur]=0;
+		q.push(cur);
+		while(q.size()) {
+			int v=q.front(); q.pop();
+			ITR(e,E[v]) if(e->cap>0 && lev[e->to]<0) lev[e->to]=lev[v]+1, q.push(e->to);
+		}
+	}
+	V dfs(int from,int to,V cf) {
+		if(from==to) return cf;
+		for(;itr[from]<E[from].size();itr[from]++) {
+			edge* e=&E[from][itr[from]];
+			if(e->cap>0 && lev[from]<lev[e->to]) {
+				V f=dfs(e->to,to,min(cf,e->cap));
+				if(f>0) {
+					e->cap-=f;
+					E[e->to][e->reve].cap += f;
+					return f;
+				}
+			}
+		}
+		return 0;
+	}
+	V maxflow(int from, int to) {
+		V fl=0,tf;
+		while(1) {
+			bfs(from);
+			if(lev[to]<0) return fl;
+			ZERO(itr);
+			while((tf=dfs(from,to,numeric_limits<V>::max()))>0) fl+=tf;
+		}
+	}
+};
