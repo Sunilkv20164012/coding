0f0cbd8fa860934a8ecb0ff2658a815fddf397f8
Sunil <sunilkv20164012@gmail.com>
2020-04-05 23:57:40 +0530

    Update binary_lifting.cpp

--- binary_lifting.cpp
+++ binary_lifting.cpp
@@ -1,48 +1,130 @@
-int n, l;
-vector<vector<int>> adj;
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+#define pb push_back
+#define fr first
+#define sc second
+const ll inf = (ll)1e18+7;
+const ll mo = (ll)1000000007;
+#define N 1000006
+using namespace std;
 
-int timer;
-vector<int> tin, tout;
-vector<vector<int>> up;
 
-void dfs(int v, int p)
+vector<ll> prime;
+ll minp[N];
+void seive(ll n)
 {
-    tin[v] = ++timer;
-    up[v][0] = p;
-    for (int i = 1; i <= l; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
+	memset(minp,0,sizeof(minp));
+	for(ll i=2;i<=n;i++)
+	{
+		if(minp[i]==0)
+		{
+			prime.push_back(i);
+			for(ll j=i;j<=n;j+=i)
+			{
+				minp[j]=i;
+			}
+		}
+	}
+	return;
+}
 
-    for (int u : adj[v]) {
-        if (u != p)
-            dfs(u, v);
+vector<ll> g[N];
+ll a[N], lvl[N], P[N][22];
+
+void dfs(ll u, ll par){
+    lvl[u] = 1+lvl[par];
+    P[u][0] = par;
+	for(ll i=0;i<g[u].size();i++){
+		ll v=g[u][i];
+		if (v == par) continue;
+		dfs(v, u);
+	}
+}
+
+ll lca(ll u, ll v){
+    ll i, lg;
+    if (lvl[u] < lvl[v]) swap(u, v);
+
+    for(lg = 0; (1<<lg) <= lvl[u]; lg++);
+    lg--;
+
+    for(i=lg; i>=0; i--){
+        if (lvl[u] - (1<<i) >= lvl[v])
+            u = P[u][i];
     }
 
-    tout[v] = ++timer;
-}
+    if (u == v)
+    	return u;
 
-bool is_ancestor(int u, int v)
-{
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
-}
-
-int lca(int u, int v)
-{
-    if (is_ancestor(u, v))
-        return u;
-    if (is_ancestor(v, u))
-        return v;
-    for (int i = l; i >= 0; --i) {
-        if (!is_ancestor(up[u][i], v))
-            u = up[u][i];
+    for(i=lg; i>=0; i--){
+        if (P[u][i] != -1 and P[u][i] != P[v][i])
+            u = P[u][i], v = P[v][i];
     }
-    return up[u][0];
+
+    return P[u][0];
 }
 
-void preprocess(int root) {
-    tin.resize(n);
-    tout.resize(n);
-    timer = 0;
-    l = ceil(log2(n));
-    up.assign(n, vector<int>(l + 1));
-    dfs(root, root);
+ll getAncestor(ll u, ll dis){
+    dis = lvl[u] - dis;
+    ll i, lg = 0;
+    for(; (1<<lg) <= lvl[u]; lg++); lg--;
+
+    for(i=lg; i>=0; i--){
+        if (lvl[u] - (1<<i) >= dis)
+            u = P[u][i];
+    }
+
+    return u;
+}
+
+ll dis(ll u, ll v){
+    if (lvl[u] < lvl[v]) swap(u, v);
+    ll w = lca(u, v);
+    return lvl[u] + lvl[v] - 2*lvl[w];
+}
+
+int main()
+{
+	ll t;
+	cin>>t;
+	seive(1000000);
+	while(t--)
+	{
+		ll i,j,n,q,u,v;
+		cin>>n;
+		ll arr[n+1];
+		for(i=0;i<n-1;i++)
+		{
+			cin>>u>>v;
+			g[u].push_back(v);
+			g[v].push_back(u);
+		}
+		for(i=1;i<=n;i++)
+		{
+			cin>>arr[i];
+		}
+		for(i=0;i<21;i++){
+			for(j=0;j<n+1;j++){
+				P[j][i]=-1;
+			}
+		}
+		lvl[0] = -1;
+
+		dfs(1, 0);
+
+		for(i=1; i<21; i++){
+	      for(j=1;j<n+1;j++){
+	         if (P[j][i-1] != -1)
+	          P[j][i] = P[P[j][i-1]][i-1];
+	      }
+		}
+		cin>>q;
+		while(q--)
+		{
+			cin>>u>>v;
+			cout<<(lca(u,v))<<endl;
+		}
+	}
+	return 0;
 }
